705b2a04d52999a1aa90012ade81ab306ea12031
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-03-20 03:31:53 +0200

    Add choose command

---
+++ modules/choose.py
@@ -0,0 +1,20 @@
+import random
+import discord
+from modules.common import get_hex_colour
+
+async def choose(message):
+    prefixs = "!c choose "
+    repl = message.content[9:]
+    args = repl.strip().split("|")
+    valid = [x for x in args if x.strip() != '']
+
+    if len(valid) < 2:
+        msg = "**I need at least 2 options! Syntax: [option1] | [option2] ...**"
+    else:
+        random_n = random.randint(0, len(valid)-1)
+        chosen = valid[random_n]
+        msg = "**"+ message.author.display_name + ", I choose '" + chosen.strip()+"'!**"
+
+    
+    emb = discord.Embed(title=msg, color=get_hex_colour(cora_eye=True))
+    await message.channel.send(embed=emb)
