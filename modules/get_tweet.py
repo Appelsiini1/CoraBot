a7a4068e1de53f233b05a5527cd691a2308493e6
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-03-22 00:05:23 +0200

    Star of tweet command

---
+++ modules/get_tweet.py
@@ -0,0 +1,19 @@
+import tweepy
+import discord
+import logging
+import random
+
+MAX_ITEMS = 100
+
+async def get_tweet(message, auth):
+    api = tweepy.API(auth)
+
+    twitter_handle = message.content.split(" ")[2]
+    cursor = tweepy.Cursor(api.user_timeline, id=twitter_handle).items(MAX_ITEMS)
+
+    random_n = random.randint(0, 99)
+
+    for i in range(0, random_n):
+        tweet = cursor.next()
+
+    
