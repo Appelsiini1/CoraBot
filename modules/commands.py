461be1ae57d3ed56b74bf18f692831a5f7d4e56a
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-04-06 01:24:37 +0300

    Add new commands

--- modules/commands.py
+++ modules/commands.py
@@ -12,9 +12,13 @@ f (alias: F)
 choose [option1 | option2 | ...]
 vacc [Area code (or empty for all areas) | help]
 poll [new|end|help]
+vote help
 
 Admin commands:
 giveaway
-endgiveaway [GiveawayID]"""
+endgiveaway [GiveawayID]
+poll [set|edit|del]role
+poll new -r
+"""
     emb = discord.Embed(title="List of available commands:", description=cmd_list, colour=common.get_hex_colour(cora_blonde=True))
     await message.channel.send(embed=emb)
