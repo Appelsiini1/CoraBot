63e9ebd0088761615d0fe2ffa05413bab9ae419a
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-04-21 02:15:08 +0300

    Add guild join message
    - Added a welcome message sent to the server owner
    - Changed formatting in command help
    - Bump version number

--- main.py
+++ main.py
@@ -106,12 +106,37 @@ async def on_message(message):
     elif cmd == "pop":
         await pop.pop(message)
     elif cmd == "mood":
-        await message.channel.send("https://cdn.discordapp.com/attachments/816694548457324544/830847194142605403/hui_saakeli_tata_elamaa.mp4")
+        await message.channel.send(
+            "https://cdn.discordapp.com/attachments/816694548457324544/830847194142605403/hui_saakeli_tata_elamaa.mp4"
+        )
     elif cmd == "nitro":
         await nitro.nitroJunction(message)
 
     else:
         await message.channel.send("What was that?")
+
+
+@client.event
+async def on_guild_join(guild):
+    emb = discord.Embed()
+    emb.color = common.get_hex_colour(cora_blonde=True)
+    emb.title = "Hey there!"
+    emb.description = "Hi! Thanks for adding me to your server! <3\n\
+        You can use my features by typing commands with the prefix `!c`. To access a list of available commands, use `!c help`.\n\
+        \nPlease make sure I have the proper rights, especially to view the channels you want me to listen for commands in, send messages & embed links.\n\
+        \n\
+        Sincerely,\n\
+        ~ Cora ~"
+    emb.set_thumbnail(
+        url="https://media.discordapp.net/attachments/693166291468681227/834200862246043648/cora_pfp.png"
+    )
+    try:
+        dm_channel = guild.owner.dm_channel
+        if dm_channel == None:
+            dm_channel = await guild.owner.create_dm()
+        await dm_channel.send(embed=emb)
+    except Exception:
+        logging.exception("Vould not send welcome message to server owner.")
 
 
 client.run(DISCORD_TOKEN)
--- modules/commands.py
+++ modules/commands.py
@@ -18,13 +18,13 @@ vote help
 vote [Poll ID] [option:votes], [option:votes], ...
 pop [number between 1-14]
 
-Admin commands:
+**_Admin commands:_**
 giveaway
 endgiveaway [GiveawayID]
 poll [set|del]roles
 poll roles
 poll new -r
-nitro [add|del|start|stop|notice]
+nitro [add|del|get|start|stop|notice]
 """
     emb = discord.Embed(
         title="List of available commands:",
