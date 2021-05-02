d2708ce119786c66bbd08a01e0bf51db4eff0a7e
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-05-03 01:09:44 +0300

    Add initiation script
    This script should be used before starting the bot so necessary tables are created.

---
+++ Other scripts/Tracked table.py
@@ -0,0 +1,21 @@
+# This script is used to add the 'Tracked' table to the bots database as the bot needs the table before it can initialize the others.
+# Execute this before starting the bot. This only needs to be done once though.
+
+
+import sqlite3
+
+with sqlite3.connect("Databases/database.db") as conn:
+    c = conn.cursor()
+    c.execute(
+            """CREATE TABLE IF NOT EXISTS Tracked(
+            Channel_ID INT,
+            Guild_ID INT,
+            Type INT,
+            PRIMARY KEY (Channel_ID)
+        );"""
+        )
+    # c.execute(
+    #     """ALTER TABLE Tracked
+    #     ADD COLUMN Type INT;"""
+    #     )
+    conn.commit()
