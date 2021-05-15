98de8d108ee3368d5bf07cb8bb2c9bdc6744a4d6
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-05-16 01:07:25 +0300

    Add track types to constants

--- constants.py
+++ constants.py
@@ -4,7 +4,6 @@ from sys import exit
 import sqlite3
 
 
-
 def get_tokens():
     """Gets environment variables. Returns a list."""
     try:
@@ -20,7 +19,9 @@ def get_tokens():
 PREFIX = "!c "
 AUTHOR = "This bot is maintained by Appelsiini1"
 GIT = "Source code for this bot can be found at https://github.com/Appelsiini1/CoraBot"
-DB_F = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Databases", "database.db")
+DB_F = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "Databases", "database.db"
+)
 
 # Token constants
 TOKENS = get_tokens()
@@ -28,19 +29,24 @@ DISCORD_TOKEN = TOKENS[0].lstrip("TOKEN").strip()[1:]
 TWIT_API_KEY = TOKENS[1].lstrip("API_KEY").strip()[1:]
 TWIT_API_SECRET = TOKENS[2].lstrip("API_SECRET").strip()[1:]
 
+
 class CHANNEL_TRACKER:
     def __init__(self):
-        self.channels = self.start()
+        self.channels, self.types = self.start()
+
     def update(self):
         with sqlite3.connect(DB_F) as conn:
             c = conn.cursor()
             c.execute("SELECT * FROM Tracked")
             data = c.fetchall()
             channels = []
+            types = []
             for ch in data:
                 channels.append(ch[0])
+                types.append(ch[2])
             self.channels = channels
-    
+            self.types = types
+
     def start(self):
         # DO NOT CALL MORE THAN ONCE
         with sqlite3.connect(DB_F) as conn:
@@ -48,9 +54,11 @@ class CHANNEL_TRACKER:
             c.execute("SELECT * FROM Tracked")
             data = c.fetchall()
             channels = []
+            types = []
             for ch in data:
                 channels.append(ch[0])
-            return channels
+                types.append(ch[2])
+            return channels, types
 
 
 # Keeps track of channels that need to be tracked
