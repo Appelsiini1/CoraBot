7ac9a1ed40aace77c564ddcc467e36df3e9b118d
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-05-18 23:07:57 +0300

    Progress on auction
    - Add scehduling library
    - add currency symbol library
    - add attributes to command stucture
    - add missing returns
    - add new attributes to parsing
    - add error handling for incorrect syntax on new attributes
    - separate starting the acutal auction to it's own command for use in scheduling

--- modules/auction.py
+++ modules/auction.py
@@ -1,15 +1,17 @@
 import discord
 import logging
 import datetime
-
+import sqlite3
+from currency_symbols import CurrencySymbols
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from modules.common import get_hex_colour, forbiddenErrorHandler
 from constants import DB_F, TRACKED_CHANNELS
 
 
 async def makeAuction(message):
     # Command structure
-    # !c auction start title;starting bid [int, x > 0];min increase [int, x >= 0];autobuy [int, 0 if disabled, x >= 0];start time [DD.MM.YYYY HH:MM UTC-/+HHMM (as 24-hour clock)] or [now];end time or empty
-    # Countdown example: https://www.timeanddate.com/countdown/generic?iso=20240322T1442&p0=101&msg=Event+name&font=cursive&csz=1 NOTE: Always Helsinki time
+    # !c auction start title;number of slots [int];currency as a 3-letter identifier (ISO-4217);starting bid [int, x > 0];min increase [int, x >= 0];autobuy [int, 0 if disabled, x >= 0];start time [DD.MM.YYYY HH:MM UTC-/+HHMM (as 24-hour clock)] or [now];end time or empty
+    # Countdown example: https://www.timeanddate.com/countdown/generic?iso=20240322T1442&p0=101&msg=Event+name&font=cursive&csz=1 NOTESa: Always Helsinki time (p0=101), csz=1 -> stop countdown at zero
 
     cmd_split = message.content.split(" ")
     emb = discord.Embed()
@@ -20,55 +22,84 @@ async def makeAuction(message):
 
         try:
             message.channel.send(embed=emb)
+            return
         except discord.errors.Forbidden:
             forbiddenErrorHandler(message)
             return
-    elif len(cmd_split[3].split(";")) < 6:
+    elif len(cmd_split[3].split(";")) < 8:
         emb.title = "Too few arguments. Please remember to give all arguments. See `!c auction help` for more information."
         emb.color = get_hex_colour(error=True)
 
         try:
             message.channel.send(embed=emb)
+            return
         except discord.errors.Forbidden:
             forbiddenErrorHandler(message)
             return
     else:
         args = cmd_split[3].split(";")
         title = args[0].strip()
-        start_bid = args[1].strip().lstrip("[").rstrip("]").lower()
-        min_inc = args[2].strip().lstrip("[").rstrip("]").lower()
-        autobuy = args[3].strip().lstrip("[").rstrip("]").lower()
-        start_time = args[4].strip().lstrip("[").rstrip("]").lower()
-        end_time = args[5].strip().lstrip("[").rstrip("]").lower()
+        slots = args[1].strip().lstrip("[").rstrip("]")
+        currency = args[2].strip().lstrip("[").rstrip("]").upper()
+        start_bid = args[2].strip().lstrip("[").rstrip("]")
+        min_inc = args[3].strip().lstrip("[").rstrip("]")
+        autobuy = args[4].strip().lstrip("[").rstrip("]")
+        start_time = args[5].strip().lstrip("[").rstrip("]").lower()
+        end_time = args[6].strip().lstrip("[").rstrip("]").lower()
 
         try:
             start_bid = int(start_bid)
             min_inc = int(min_inc)
             autobuy = int(autobuy)
+            slots = int(slots)
         except ValueError:
             emb.title = (
-                "Starting bid, minimum increase or autobuy value(s) are not integers."
+                "Number of slots, starting bid, minimum increase or autobuy value(s) are not integers."
             )
             emb.color = get_hex_colour(error=True)
 
             try:
                 message.channel.send(embed=emb)
+                return
+            except discord.errors.Forbidden:
+                forbiddenErrorHandler(message)
+                return
+        if start_bid == 0:
+            emb.title = "Starting bid must be greater than zero."
+            emb.color = get_hex_colour(error=True)
+            try:
+                message.channel.send(embed=emb)
+                return
+            except discord.errors.Forbidden:
+                forbiddenErrorHandler(message)
+                return
+        
+        currency_symbol = CurrencySymbols.get_symbol(currency)
+        if currency_symbol == None:
+            emb.title = "Unknown currency code."
+            emb.color = get_hex_colour(error=True)
+            try:
+                message.channel.send(embed=emb)
+                return
             except discord.errors.Forbidden:
                 forbiddenErrorHandler(message)
                 return
 
         if start_time == "now":
             start_time = datetime.datetime.today()
+            startsnow = 1
         else:
             try:
                 start_time = datetime.datetime.strptime(
                     start_time, "%d.%m.%Y %H:%M %Z%z"
                 )
+                startsnow = 0
             except ValueError:
                 emb.title = "Error parsing start time, does not match the time format `dd.mm.yyyy HH:MM UTC±HHMM`."
                 emb.color = get_hex_colour(error=True)
                 try:
                     message.channel.send(embed=emb)
+                    return
                 except discord.errors.Forbidden:
                     forbiddenErrorHandler(message)
                     return
@@ -81,10 +112,17 @@ async def makeAuction(message):
                 emb.color = get_hex_colour(error=True)
                 try:
                     message.channel.send(embed=emb)
+                    return
                 except discord.errors.Forbidden:
                     forbiddenErrorHandler(message)
                     return
 
+        with sqlite3.connect(DB_F) as conn:
+            c = conn.cursor()
+            
+        
+async def startAuction(client):
+    pass
 
 async def bid(message):
     pass
