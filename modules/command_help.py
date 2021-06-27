7c8d02bdc5d084f364f41f06f40279736e26fff8
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-06-28 01:01:26 +0300

    Move info
    Move info command from on_message event to it's own function

--- modules/command_help.py
+++ modules/command_help.py
@@ -1,8 +1,11 @@
-import discord
+from discord import Embed
+from discord.errors import Forbidden
+from discord.ext.commands import command
 import logging
 from modules import common
+from constants import VERSION
 
-
+@command(name="help")
 async def cmds(message):
     cmd_list = """`hi` (alias: `hello`)
 `help`
@@ -26,14 +29,14 @@ async def cmds(message):
 `poll new -r`
 `nitro [help | add | del | export | start | stop | notice | check | spin]`
 """
-    emb = discord.Embed(
+    emb = Embed(
         title="List of available commands:",
         description=cmd_list,
         colour=common.get_hex_colour(cora_blonde=True),
     )
     try:
         await message.channel.send(embed=emb)
-    except discord.errors.Forbidden:
+    except Forbidden:
         logging.error("Unable to send message due to 403 - Forbidden")
         emb.clear_fields()
         emb.description = f"Unable to send message to channel in '{message.guild.name}'. If you are the server owner, please make sure I have the proper rights to post messages to that channel."
@@ -42,3 +45,18 @@ async def cmds(message):
         if dm_channel == None:
             dm_channel = await message.guild.owner.create_dm()
         await dm_channel.send(embed=emb)
+
+@command()
+async def info(ctx):
+        emb = Embed()
+        emb.title = "CoraBot Info"
+        emb.description = f"**Created by** Appelsiini1\nThe source code & development info for this bot can be found at https://github.com/Appelsiini1/CoraBot\n\nVersion: {VERSION}"
+        emb.color = common.get_hex_colour(cora_blonde=True)
+        emb.set_thumbnail(
+        url="https://media.discordapp.net/attachments/693166291468681227/834200862246043648/cora_pfp.png"
+        )
+
+        try:
+            await ctx.send(embed=emb)
+        except Forbidden:
+            common.forbiddenErrorHandler(ctx)
