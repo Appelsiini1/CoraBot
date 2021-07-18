9b69ad87050929564a1c8a235b08f47e32aef146
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-07-18 22:22:22 +0300

    Work on scheduler

--- modules/scheduler.py
+++ modules/scheduler.py
@@ -2,7 +2,22 @@ from discord.ext import tasks
 from constants import *
 import sqlite3
 from pytz import utc
+from datetime import datetime, time, timedelta
 
+TEN_MINUTES = time(0, 10, 0, 0)
+ONE_MINUTE = time(0, 1, 0, 0)
+
+class EVENT_CHECKER(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @tasks.loop(minutes=1)
+    async def less_than_ten():
+        pass
+
+    @tasks.loop(seconds=10)
+    async def less_than_one():
+        pass
 
 # Scheduler event listener
 def on_scheduler_event(event):
@@ -11,12 +26,11 @@ def on_scheduler_event(event):
     else:
         logging.info("Job executed succesfully.")
 
-class SCHEDULER():
-    def __init__(self) -> None:
+class SCHEDULER(commands.Cog):
+    def __init__(self, bot) -> None:
         #self.event_checker.start()
         #self.test_loop.start()
-        pass
-
+        self.bot = bot
 
     @tasks.loop(seconds=5.0, count=3)
     async def test_loop():
@@ -26,4 +40,13 @@ class SCHEDULER():
     async def event_checker():
         with sqlite3.connect(DB_F) as conn:
             c = conn.cursor()
+            current_time = datetime.today()
+            year_now = current_time.strftime("%Y")
+            month_now = current_time.strftime("%m")
+            day_now = current_time.strftime("%d")
+            c.execute("SELECT * FROM Scheduler WHERE Year=? AND Month=? AND Day=?", (year_now, month_now, day_now))
+            data = c.fetchall()
 
+def setup(client):
+    client.add_cog(SCHEDULER(client))
+    client.add_cog(EVENT_CHECKER(client))
