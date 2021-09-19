1d12537a7b3a05bd4727e7b5c1d07012dea9bc20
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2021-09-19 13:33:43 +0300

    Change timeformat & how time parsing is handled

--- modules/auction.py
+++ modules/auction.py
@@ -3,8 +3,9 @@ import logging
 import datetime
 import sqlite3
 from currency_symbols import CurrencySymbols
+from dateutil import tz
 from discord.ext import commands
-from modules.common import get_hex_colour, forbiddenErrorHandler
+from modules.common import get_hex_colour, forbiddenErrorHandler, timeParser, timeConverter
 from constants import DB_F, TRACKED_CHANNELS
 from modules.command_help import auction_help
 
@@ -29,10 +30,10 @@ class Auction(commands.Cog):
             return
 
     # add auction to schedule and construct info message
-    async def makeAuction(self, ctx, scheduler):
+    async def makeAuction(self, ctx):
         # Command structure
-        # !c auction start title;number of slots [int];currency as a 3-letter identifier (ISO-4217);starting bid [int, x > 0];min increase [int, x >= 0];autobuy [int, 0 if disabled, x >= 0];start time [DD.MM.YYYY HH:MM UTC-/+HHMM (as 24-hour clock)] or [now];end time or empty
-        # Countdown example: https://www.timeanddate.com/countdown/generic?iso=20240322T1442&p0=101&msg=Event+name&font=cursive&csz=1 NOTESa: Always Helsinki time (p0=101), csz=1 -> stop countdown at zero
+        # !c auction start title;number of slots [int];currency as a 3-letter identifier (ISO-4217);starting bid [int, x > 0];min increase [int, x >= 0];autobuy [int, 0 if disabled, x >= 0];start time [DD.MM.YYYY HH:MM, Area/Location (as 24-hour clock)(Timezone as per the Olson database)] or [now];end time or empty
+        # Countdown example: https://www.timeanddate.com/countdown/generic?iso=20240322T1442&p0=101&msg=Event+name&font=cursive&csz=1 NOTES: Always Helsinki time (p0=101), csz=1 -> stop countdown at zero
 
         cmd_split = ctx.message.content.split(" ")
         emb = discord.Embed()
@@ -57,8 +58,8 @@ class Auction(commands.Cog):
             start_bid = args[2].strip().lstrip("[").rstrip("]")
             min_inc = args[3].strip().lstrip("[").rstrip("]")
             autobuy = args[4].strip().lstrip("[").rstrip("]")
-            start_time = args[5].strip().lstrip("[").rstrip("]").lower()
-            end_time = args[6].strip().lstrip("[").rstrip("]").lower()
+            start_time_raw = args[5].strip().lstrip("[").rstrip("]").lower()
+            end_time_raw = args[6].strip().lstrip("[").rstrip("]").lower()
 
             try:
                 start_bid = int(start_bid)
@@ -86,26 +87,24 @@ class Auction(commands.Cog):
                 await self.sendEmbed(ctx, emb)
                 return
 
-            if start_time == "now":
-                start_time = datetime.datetime.today()
+            if start_time_raw == "now":
+                start_time = datetime.datetime.today().replace(tz.gettz())
                 startsnow = 1
             else:
                 try:
-                    start_time = datetime.datetime.strptime(
-                        start_time, "%d.%m.%Y %H:%M %Z%z"
-                    )
+                    start_time = timeParser(start_time_raw)
                     startsnow = 0
                 except ValueError:
-                    emb.title = "Error parsing start time, does not match the time format `dd.mm.yyyy HH:MM UTC±HHMM`."
+                    emb.title = "Error parsing start time, does not match the time format `dd.mm.yyyy HH:MM, Area/Location` or timezone not recognized."
                     emb.color = get_hex_colour(error=True)
                     await self.sendEmbed(ctx, emb)
                     return
 
-            if end_time != "":
+            if end_time_raw != "":
                 try:
-                    end_time = datetime.datetime.strptime(end_time, "%d.%m.%Y %H:%M")
+                    end_time = timeParser(end_time_raw)
                 except ValueError:
-                    emb.title = "Error parsing end time, does not match the time format `dd.mm.yyyy HH:MM UTC±HHMM`."
+                    emb.title = "Error parsing end time, does not match the time format `dd.mm.yyyy HH:MM, TIMEZONE` or timezone not recognized."
                     emb.color = get_hex_colour(error=True)
                     await self.sendEmbed(ctx, emb)
                     return
